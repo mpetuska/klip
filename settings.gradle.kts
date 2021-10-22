e24c3880e19424dfacca4fa97cf1d6657d58b83b
Martynas Petuška <martynas@petuska.dev>
2021-10-22 20:53:18 +0100

    versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,16 +7,15 @@ pluginManagement {
 
 plugins {
   id("de.fayard.refreshVersions") version "0.23.0"
-  id("com.gradle.enterprise") version "3.7"
-////                      # available:"3.7.1"
+  id("com.gradle.enterprise") version "3.7.1"
 }
 
 rootProject.name = "klip"
 
 include(
-    ":library:klip-core",
-    ":library:klip-api",
-    ":plugin:klip-gradle-plugin",
-    ":plugin:klip-kotlin-plugin",
-    ":plugin:klip-kotlin-plugin-native",
+  ":library:klip-core",
+  ":library:klip-api",
+  ":plugin:klip-gradle-plugin",
+  ":plugin:klip-kotlin-plugin",
+  ":plugin:klip-kotlin-plugin-native",
 )
